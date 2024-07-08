429559404e8fe9f82362f2caa52d0dce63ea29d9
kunalarya873 <kunalarya873@gmail.com>
2024-07-09 00:54:06 +0530

    Adding basic project

--- timepass/users/forms.py
+++ timepass/users/forms.py
@@ -1,66 +1,69 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
 from .models import Profile
 
-
 class RegisterForm(UserCreationForm):
-    # fields we want to include and customize in our form
-    first_name = forms.CharField(max_length=100,
-                                 required=True,
-                                 widget=forms.TextInput(attrs={'placeholder': 'First Name',
-                                                               'class': 'form-control',
-                                                               }))
-    last_name = forms.CharField(max_length=100,
-                                required=True,
-                                widget=forms.TextInput(attrs={'placeholder': 'Last Name',
-                                                              'class': 'form-control',
-                                                              }))
-    username = forms.CharField(max_length=100,
-                               required=True,
-                               widget=forms.TextInput(attrs={'placeholder': 'Username',
-                                                             'class': 'form-control',
-                                                             }))
-    email = forms.EmailField(required=True,
-                             widget=forms.TextInput(attrs={'placeholder': 'Email',
-                                                           'class': 'form-control',
-                                                           }))
-    password1 = forms.CharField(max_length=50,
-                                required=True,
-                                widget=forms.PasswordInput(attrs={'placeholder': 'Password',
-                                                                  'class': 'form-control',
-                                                                  'data-toggle': 'password',
-                                                                  'id': 'password',
-                                                                  }))
-    password2 = forms.CharField(max_length=50,
-                                required=True,
-                                widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password',
-                                                                  'class': 'form-control',
-                                                                  'data-toggle': 'password',
-                                                                  'id': 'password',
-                                                                  }))
+    TYPES_OF_USERS = [
+        ('patient', 'Patient'),
+        ('doctor', 'Doctor')
+    ]
+    user_type = forms.ChoiceField(choices=TYPES_OF_USERS, widget=forms.Select(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'class': 'form-control'}))
+    avatar = forms.ImageField(required=False, widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+    username = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'form-control'}))
+    password1 = forms.CharField(max_length=50, required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control', 'data-toggle': 'password', 'id': 'password'}))
+    password2 = forms.CharField(max_length=50, required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password', 'class': 'form-control', 'data-toggle': 'password', 'id': 'password'}))
+    address_line1 = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Address Line 1', 'class': 'form-control'}))
+    city = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'placeholder': 'City', 'class': 'form-control'}))
+    state = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'placeholder': 'State', 'class': 'form-control'}))
+    pincode = forms.CharField(max_length=6, required=True, widget=forms.TextInput(attrs={'placeholder': 'Pincode', 'class': 'form-control'}))
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        fields = ['user_type', 'first_name', 'last_name', 'avatar', 'username', 'email', 'password1', 'password2', 'address_line1', 'city', 'state', 'pincode']
 
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+        user.username = self.cleaned_data['username']
+        user.set_password(self.cleaned_data['password1'])
+    
+        if commit:
+            user.save()
+
+            # Create or update Profile instance with UUID
+            profile, created = Profile.objects.get_or_create(
+                user=user,
+                defaults={
+                    'user_type': self.cleaned_data['user_type'],
+                    'address_line1': self.cleaned_data['address_line1'],
+                    'city': self.cleaned_data['city'],
+                    'state': self.cleaned_data['state'],
+                    'pincode': self.cleaned_data['pincode'],
+                    'avatar': self.cleaned_data.get('avatar', None)
+                }
+            )
+
+            if not created:
+                profile.user_type = self.cleaned_data['user_type']
+                profile.address_line1 = self.cleaned_data['address_line1']
+                profile.city = self.cleaned_data['city']
+                profile.state = self.cleaned_data['state']
+                profile.pincode = self.cleaned_data['pincode']
+                profile.avatar = self.cleaned_data.get('avatar', None)
+                profile.save()
+
+        return user
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(max_length=100,
-                               required=True,
-                               widget=forms.TextInput(attrs={'placeholder': 'Username',
-                                                             'class': 'form-control',
-                                                             }))
-    password = forms.CharField(max_length=50,
-                               required=True,
-                               widget=forms.PasswordInput(attrs={'placeholder': 'Password',
-                                                                 'class': 'form-control',
-                                                                 'data-toggle': 'password',
-                                                                 'id': 'password',
-                                                                 'name': 'password',
-                                                                 }))
-    remember_me = forms.BooleanField(required=False)
+    username = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'}))
+    password = forms.CharField(max_length=50, required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control', 'data-toggle': 'password', 'id': 'password', 'name': 'password'}))
+    remember_me = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
 
     class Meta:
         model = User
@@ -68,21 +71,50 @@ class LoginForm(AuthenticationForm):
 
 
 class UpdateUserForm(forms.ModelForm):
-    username = forms.CharField(max_length=100,
-                               required=True,
-                               widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(required=True,
-                             widget=forms.TextInput(attrs={'class': 'form-control'}))
-
     class Meta:
         model = User
         fields = ['username', 'email']
 
 
 class UpdateProfileForm(forms.ModelForm):
-    avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+    avatar = forms.ImageField(required=False, widget=forms.FileInput(attrs={'class': 'form-control-file'}))
     bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
-
+    address_line1 = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    city = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    state = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    pincode = forms.CharField(max_length=6, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    TYPES_OF_USERS = [
+        ('patient', 'Patient'),
+        ('doctor', 'Doctor')
+    ]
+    user_type = forms.ChoiceField(choices=TYPES_OF_USERS, widget=forms.Select(attrs={'class': 'form-control'}))
+    
     class Meta:
         model = Profile
-        fields = ['avatar', 'bio']
+        fields = ['avatar', 'bio', 'address_line1', 'city', 'state', 'pincode', 'user_type']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['user_type'].choices = self.TYPES_OF_USERS
+        if self.instance:
+            self.fields['user_type'].initial = self.instance.user_type
+            self.fields['bio'].initial = self.instance.bio
+            self.fields['address_line1'].initial = self.instance.address_line1
+            self.fields['city'].initial = self.instance.city
+            self.fields['state'].initial = self.instance.state
+            self.fields['pincode'].initial = self.instance.pincode
+            self.fields['avatar'].initial = self.instance.avatar
+    
+    def save(self, commit=True):
+        profile = super().save(commit=False)
+        profile.bio = self.cleaned_data['bio']
+        profile.address_line1 = self.cleaned_data['address_line1']
+        profile.city = self.cleaned_data['city']
+        profile.state = self.cleaned_data['state']
+        profile.pincode = self.cleaned_data['pincode']
+        profile.user_type = self.cleaned_data['user_type']
+        if commit:
+            profile.save()
+        return profile
+    
+
--- timepass/users/models.py
+++ timepass/users/models.py
@@ -1,25 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+import uuid
 
-
-# Extending User Model Using a One-To-One Link
 class Profile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
     avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
-    bio = models.TextField()
+    bio = models.TextField(blank=True)
+    address_line1 = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=30, blank=True)
+    state = models.CharField(max_length=30, blank=True)
+    pincode = models.CharField(max_length=6, blank=True)
+    
+    # Define choices for user_type field
+    USER_TYPES = [
+        ('patient', 'Patient'),
+        ('doctor', 'Doctor')
+    ]
+    user_type = models.CharField(max_length=10, choices=USER_TYPES)
 
     def __str__(self):
         return self.user.username
 
-    # resizing images
     def save(self, *args, **kwargs):
-        super().save()
+        super().save(*args, **kwargs)
 
+        # Resize avatar image if it exceeds 100x100 pixels
         img = Image.open(self.avatar.path)
-
         if img.height > 100 or img.width > 100:
-            new_img = (100, 100)
-            img.thumbnail(new_img)
+            new_img_size = (100, 100)
+            img.thumbnail(new_img_size)
             img.save(self.avatar.path)
